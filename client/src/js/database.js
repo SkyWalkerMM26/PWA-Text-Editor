9e9e46c19e704b9e9b16fbe0a290400a5e4a73b5
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-12-01 10:52:55 -0800

    add files and remove to edit config.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,20 +14,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-
   const jate = await openDB('jate', 1);
   const tx = jate.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ jate: content });
   const result = await request;
   console.log('Data is saved into DB', result);
   };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-  
   const jate = await openDB('jate', 1);
   const tx = jate.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Jate',
+        title: 'JATE',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -30,7 +30,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'Jate',
+        short_name: 'JATE',
         description: 'Text Editor with offline capabilities using IndexedDB',
         background_color: '#225ca3',
         theme_color: '#225ca3',
@@ -50,16 +50,12 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
